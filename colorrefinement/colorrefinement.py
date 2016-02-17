85988c713b867e56da85bebcdc974161b28cbdfb
Wouter Bos <wouwouwou@users.noreply.github.com>
2016-02-17 16:05:48 +0100

    Added doc for neighbouring-refinement

--- colorrefinement/colorrefinement.py
+++ colorrefinement/colorrefinement.py
@@ -11,3 +11,40 @@ def colorrefinement(G):
     for v in G.V():
         v.set_color_tag(v.deg())
     return G
+
+
+def checkneighbours(g):
+    """
+    max_color = g.get_max_color()
+    next_color = max_color + 1
+    i = 1
+    while i <= max_color:
+        v = g.get_nodes_color_i(i)
+        if len(v) != 1:
+            g, nextcolor = herindeel(g, v, next_color)
+        i++
+
+    """
+    for v in g.V():
+        pass
+    pass
+
+
+def herindeel(g, v, next_color):
+    """
+    a = v[0]
+    na = a.get_neighbour_colors()
+    differs = list()
+    changed = False
+    i = 1
+    while i < len(v):
+        b = v[i]
+        nb = b.get_neighbour_colors()
+        if nb != na:
+            b.tag = next_color
+            changed = True
+    if changed:
+        next_color += 1
+    return g, next_color
+    """
+    pass
