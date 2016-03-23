2741597302ea1b61571c3bbfa6594f612c955fe9
Wouter Bos <wouwouwou@users.noreply.github.com>
2016-03-23 11:08:54 +0100

    Changed order of functions for readability

--- graphcomparison/graphcomparison.py
+++ graphcomparison/graphcomparison.py
@@ -1,57 +1,3 @@
-def isbalanced(g, h):
-    """
-    Returns true if the colorings of graph g, graph h are equal
-
-    :param g: graph g
-    :param h: graph h
-    :return: true if colorings of g and h are equal
-    """
-    cg = g.getcoloring()
-    ch = h.getcoloring()
-    if len(list(cg.keys())) != len(list(ch.keys())):
-        return False
-    for c in cg:
-        if len(cg[c]) != len(ch[c]):
-            return False
-    return True
-
-
-def definesbijection(g, h):
-    """
-    Return true if graph g and graph h define a bijection
-
-    :param g: graph g
-    :param h: graph h
-    :return: tru if g and h define a bijection
-    """
-    if not isbalanced(g, h):
-        return False
-    cg = g.getcoloring()
-    ch = h.getcoloring()
-    for c in cg:
-        if len(cg[c]) != 1 and len(ch[c]) != 1:
-            return False
-    return True
-
-
-def isomorphic(g, h):
-    """
-    Returns true if graph g and graph h are isomorphic
-
-    It temporarily returns False if graph g and graph h are balanced but not define a bijection
-
-    :param g: graph g
-    :param h: graph h
-    :return: true if g and h are isomorphic
-    """
-    if not isbalanced(g, h):
-        return False
-    elif definesbijection(g, h):
-        return True
-    # todo: implement individualisation refinement for finding an isomorphism
-    return False
-
-
 def processgraphlist(graphlist):
     # todo      question: do single graphs have to be included? Do we have to get the number of
     # todo      automorphisms from this graph to itself?
@@ -95,6 +41,60 @@ def processing(graphdict):
         i += 1
 
     return nextdict, isogroup, aut
+
+
+def isomorphic(g, h):
+    """
+    Returns true if graph g and graph h are isomorphic
+
+    It temporarily returns False if graph g and graph h are balanced but not define a bijection
+
+    :param g: graph g
+    :param h: graph h
+    :return: true if g and h are isomorphic
+    """
+    if not isbalanced(g, h):
+        return False
+    elif definesbijection(g, h):
+        return True
+    # todo: implement individualisation refinement for finding an isomorphism
+    return False
+
+
+def isbalanced(g, h):
+    """
+    Returns true if the colorings of graph g, graph h are equal
+
+    :param g: graph g
+    :param h: graph h
+    :return: true if colorings of g and h are equal
+    """
+    cg = g.getcoloring()
+    ch = h.getcoloring()
+    if len(list(cg.keys())) != len(list(ch.keys())):
+        return False
+    for c in cg:
+        if len(cg[c]) != len(ch[c]):
+            return False
+    return True
+
+
+def definesbijection(g, h):
+    """
+    Return true if graph g and graph h define a bijection
+
+    :param g: graph g
+    :param h: graph h
+    :return: tru if g and h define a bijection
+    """
+    if not isbalanced(g, h):
+        return False
+    cg = g.getcoloring()
+    ch = h.getcoloring()
+    for c in cg:
+        if len(cg[c]) != 1 and len(ch[c]) != 1:
+            return False
+    return True
 
 
 def countautomorphisms(g):
