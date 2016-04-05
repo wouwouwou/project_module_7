12e71242e1e92c87bf32dcd701ee6f3beb8d1a50
Wouter Bos <wouwouwou@users.noreply.github.com>
2016-04-05 15:50:43 +0200

    Code cleanup part 1

--- colorrefinement/colorrefinement.py
+++ colorrefinement/colorrefinement.py
@@ -7,106 +7,6 @@ Methods for colorrefinement of graphs. Also can decide of graphs are isomorphic.
 """
 
 
-def definesbijectionvertex(v, v1):
-    if isbalancedvertex(v, v1):
-        i = 0
-        cv = getcoloringvertex(v)
-        while i < len(cv) - 1:
-            if cv[i] == cv[i + 1]:
-                return False
-            i += 1
-        return True
-    return False
-
-
-def isbalancedvertex(v, v1):
-    """
-     Returns true if the colorings of vertices v, vertices v1 are equal.
-     :param v: vertices
-     :param v1: vertices
-     :return: true if colorings of v and v1 are equal
-    """
-    cv = getcoloringvertex(v)
-    cv1 = getcoloringvertex(v1)
-    return cv == cv1
-
-
-def getcoloringvertex(v):
-    """
-     Returns a sorted coloring of vertices v
-     :param v: vertices v
-     :return: sorted coloring of vertices v
-    """
-    coloring = []
-    for v in v:
-        coloring.append(v.getcolornum())
-    msintlist(coloring)
-    return coloring
-
-
-def disjointuniongraphs(g: graph, h: graph):
-    """
-     Create a disjoint union of two graphs.
-     :param g: The first graph
-     :param h: The second graph
-     :return: The two graphs combined
-     """
-    f = graph(len(g.V()) + len(h.V()))
-    combinedlist = g.V() + h.V()
-    for i in range(len(combinedlist)):
-        v = combinedlist[i]
-        for e in v.getinclist():
-            if e.tail() == v:
-                f.addedge(f[i], f[combinedlist.index(e.head())])
-
-    colorrefinement(f)
-    return f
-
-
-def disjointunionvertices(d, i):
-    f = graph(len(d) + len(i))
-    combinedlist = d + i
-    for i in range(len(combinedlist)):
-        v = combinedlist[i]
-        for e in v.getinclist():
-            if e.tail() == v:
-                f.addedge(f[i], f[combinedlist.index(e.head())])
-
-    colorrefinement(f)
-    return f
-
-
-def getcoloringdict(v):
-    resultdict = dict()
-    maximum = 0
-    for v in v:
-        if resultdict.get(v.getcolornum()) is None:
-            resultdict[v.getcolornum()] = set()
-        resultdict[v.getcolornum()].add(v)
-        maximum = max(maximum, v.getcolornum())
-    return resultdict, maximum
-
-
-def countiso(g, h):
-    colorrefinement(g)
-    colorrefinement(h)
-    return countisomorphism(g.V(), h.V())
-
-
-def makegraphfromvertices(d):
-    g = graph(len(d))
-    for i in range(len(d)):
-        v = d[i]
-        g.V()[i].setcolornum(d[i].getcolornum())
-        for e in v.getinclist():
-            if e.tail() == v:
-                g.addedge(g[i], g[d.index(e.head())])
-
-    colorrefinement(g)
-
-    return g
-
-
 def colorrefinement(g):
     """
     Gives a stable coloring to graph g
@@ -122,16 +22,6 @@ def slowcolorrefinement(g):
     refbydeg(g)
     refbynbs(g)
     return g
-
-
-"""
-def deepcopy(obj):
-    if isinstance(obj, dict):
-        return {deepcopy(key): deepcopy(value) for key, value in obj.items()}
-    if hasattr(obj, '__iter__'):
-        return type(obj)(deepcopy(item) for item in obj)
-    return obj
-"""
 
 
 def pruneandnumberisos(g):
